de42ef7b46cb402c8216545516f34a54add7a2cf
ManuboluR <154876785+ManuboluR@users.noreply.github.com>
2023-12-26 14:29:07 +0530

    Add files via upload

---
+++ src/test/java/Utilities/WaitHelper.java
@@ -0,0 +1,25 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class WaitHelper {
+    public WebDriver driver;
+
+    public WaitHelper(WebDriver rdriver){
+        this.driver=rdriver;
+    }
+    public void WaitForElement(WebElement element){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOf(element));
+
+    }
+
+
+}
---
+++ src/test/java/Utilities/contetnts.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+public class contetnts {
+    public static String password="test@123";
+    public static String searchEmail="victoria_victoria@nopCommerce.com";
+    public static String searcfirstName="Victoria";
+    public static String searcLatName="Terces";
+    public static String addcustompageFirstName="Revanth";
+    public static String addcustompageLastName="Manu";
+    public static String addcustompagedateofBirth="7/05/2023";
+    public static String addcustompageCompanyName="Cognizant";
+    public static String addcustompageadmincomments="ADhbshjkdj";
+
+
+}
