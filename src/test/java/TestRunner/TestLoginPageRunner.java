fd7662d92e36c4b63e28a893acb7acfa28d0c364
ManuboluR <154876785+ManuboluR@users.noreply.github.com>
2023-12-26 14:27:21 +0530

    Add files via upload

--- src/test/java/TestRunner/TestLoginPageRunner.java
+++ src/test/java/TestRunner/TestLoginPageRunner.java
@@ -1,17 +1,16 @@
 package TestRunner;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C:\\Users\\850999\\IdeaProjects\\CucumberFrameWork\\Features\\Customers.feature",
+        features = "C:\\Users\\850999\\IdeaProjects\\CucumberFrameWork\\Features\\Login.feature",
         glue = "StepDefinations",
         dryRun = false,
-        //monochrome = true,
+        monochrome = true,
         plugin = {"pretty","html:target/cucumber-reports/Cucumber.html"}
-
-        )
-
-public class TestCustomerPageRunner {
+)
+public class TestLoginPageRunner {
 }
