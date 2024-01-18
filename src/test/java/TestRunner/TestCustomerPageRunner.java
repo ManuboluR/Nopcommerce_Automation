91c37cf03890f3a52e1e97f671ea834dccacb162
ManuboluR <154876785+ManuboluR@users.noreply.github.com>
2024-01-18 22:07:23 +0530

    Update TestCustomerPageRunner.java

--- src/test/java/TestRunner/TestCustomerPageRunner.java
+++ src/test/java/TestRunner/TestCustomerPageRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C:\\Users\\850999\\IdeaProjects\\CucumberFrameWork\\Features\\Customers.feature",
+        features = "C://Users//850999//IdeaProjects//CucumberFrameWork//Features//Customers.feature",
         glue = "StepDefinations",
         dryRun = false,
         //monochrome = true,
