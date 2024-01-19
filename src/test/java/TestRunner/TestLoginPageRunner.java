4b4fca89ca35673880f7e6ce06e8495f9c9adc5f
ManuboluR <154876785+ManuboluR@users.noreply.github.com>
2024-01-19 22:31:19 +0530

    Update TestLoginPageRunner.java

--- src/test/java/TestRunner/TestLoginPageRunner.java
+++ src/test/java/TestRunner/TestLoginPageRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "/root/Features/Login.feature",
+        features = "/home/ubuntu/Features/Login.feature",
         glue = "StepDefinations",
         dryRun = false,
         monochrome = true,
